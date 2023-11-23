7531bc8394c33f0e6dcb4bc77852ab4e005b84a0
Walter N <2760walter@gmail.com>
2023-11-23 11:30:14 +0300

    Complete assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,29 @@
 #!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    
+    
+    fibs = []
+    if length == 0:
+        print(fibs)
+        return fibs
+    
+    a,b =0,1
+    for _ in range(length):
+        fibs.append(a)
+        a,b = b,a+b
+        
+        
+    print(fibs)
+        
+    # elif length ==1:  
+    #     fibs.append(0)
+    #     
+    # elif length ==2:
+    #     fibs.append(0)
+    #     fibs.append(1)
+    #     
+        
+    # if length > 2:
+    #      print_fibonacci(length -1) + print_fibonacci(length-2)
+         
+    # 
